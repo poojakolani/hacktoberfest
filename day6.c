fdecb7ad7f6e6cec939a6354919268466c0d48c4
poojakolani <92969943+poojakolani@users.noreply.github.com>
2021-10-22 11:18:44 +0530

    Add files via upload

---
+++ day6.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+int main()
+{
+int n,n1;
+scanf("%d %d",&n,&n1);
+if(1<=n<=9)
+{
+if(n==1)
+printf("one");
+else if(n==2)
+printf("two");
+else if(n==3)
+printf("three");
+else if(n==4)
+printf("four");
+else if(n==5)
+printf("five");
+else if(n==6)
+printf("six");
+else if(n==7)
+printf("seven");
+else if(n==8)
+printf("eight");
+else if(n==9)
+printf("nine");
+}
+if(n>9)
+printf("Greter than 9");
+return 0;
+}
